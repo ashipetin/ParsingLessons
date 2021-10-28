04adad4ca5e86373bb8e03f751ecc4cfe6903eca
Alex <ashipetin@gmail.com>
2021-10-28 20:56:03 +0300

    1st Lesson

--- Lesson 1 Task 1.py
+++ Lesson 1 Task 1.py
@@ -1,6 +1,6 @@
 import requests
 username = 'ashipetin'
-token = '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'
+token = '###TOKEN###'
 repos = requests.get('https://api.github.com/user/repos', auth=(username, token))
 j_data = repos.json()
 # pprint(j_data)
--- Lesson 1 Task 2.py
+++ Lesson 1 Task 2.py
@@ -1,3 +1,12 @@
 import requests
 from pprint import pprint
-data = requests.get()
+url = 'https://api.vk.com/method/groups.get'
+access_token = '###TOKEN###'
+v = '5.81'
+user_id = '5977095'
+response = requests.get(url, params={
+    'user_id':user_id,
+    'access_token':access_token,
+    'v':v})
+result = response.json()
+pprint(result)
